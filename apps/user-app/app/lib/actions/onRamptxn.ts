7fb8e017e18986ebe306ca543aa3b55517947624
Pratham khatri <khatripratham349@gmail.com>
2024-08-06 00:53:57 +0530

    first_commit

---
+++ apps/user-app/app/lib/actions/onRamptxn.ts
@@ -0,0 +1,35 @@
+
+"use server"
+
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import prisma from "@repo/db/client";
+
+
+export const onRampTransaction = async (amount: number, provider: string) => {
+    
+   const session = await getServerSession(authOptions);
+    const userId = session?.user?.id; 
+    if (!userId) {
+        throw new Error("User not found");
+    }
+    const token = Math.random().toString(36).substring(7);
+
+     await prisma.onRampTransaction.create({
+        data: {
+            amount,
+            provider,
+            userId:Number(userId),
+            status: "Processing",
+            startTime: new Date(),
+            token:token
+        }
+    });
+
+
+    return {
+        message: "Transaction initiated",
+    }
+}
+
